9d641b0185c7f5b73c277aad2c5d38d06f6ea0f5
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2024-12-23 11:35:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите цифру 1 для открытия блокнота");
+        string userInput = Console.ReadLine();
+        if (userInput == "1")
+        {
+            System.Diagnostics.Process.Start("notepad.exe");
+            Console.WriteLine("Блокнот открыт");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка");
+        }
+    }
+}
